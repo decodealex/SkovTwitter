a7a0be42d9f9d35a8b7e00f4d7a270d9cf9c6043
AlexKov <kovalyshyn.alex@gmail.com>
2020-05-19 13:17:58 +0300

    Created Notification data model.

---
+++ SkovTwitter/Models/Notification.swift
@@ -0,0 +1,51 @@
+//
+//  Notification.swift
+//  SkovTwitter
+//
+//  Created by Oleksandr Kovalyshyn on 19.05.2020.
+//  Copyright © 2020 Oleksandr Kovalyshyn. All rights reserved.
+//
+
+import UIKit
+
+enum NotificationType: Int {
+    case follow
+    case like
+    case reply
+    case retweet
+    case mention
+}
+
+struct Notification {
+    
+    // MARK: - Properties
+    
+    let tweetID: String?
+    var timestamp: Date!
+    let user: User
+    var tweet: Tweet?
+    var type: NotificationType!
+    
+    // MARK: - Lifecycle
+    
+    init(user: User, tweet: Tweet?, dictionary: [String: AnyObject]) {
+        self.user = user
+        self.tweet = tweet
+        
+        self.tweetID = dictionary["tweetID"] as? String ?? ""
+        
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        if let type = dictionary["type"] as? Int {
+            self.type = NotificationType(rawValue: type)
+        }
+    }
+    
+    // MARK: - API
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+}
